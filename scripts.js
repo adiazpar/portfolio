06514c661db607b9031afdd367e42ca2919d60b8
adiazpar <alexdiaz0923@gmail.com>
2025-10-08 22:57:51 -0600

    Pushing to test new email functionality

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
 // Fade in effect:
-
 document.addEventListener('DOMContentLoaded', function() {
     const fadeElements = document.querySelectorAll('.fade-in');
 
@@ -9,3 +8,26 @@ document.addEventListener('DOMContentLoaded', function() {
         }, index * 1);
     });
 });
+
+// Email functionality:
+function sendMail() {
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+    };
+
+    const serviceID = "service_trhlufi";
+    const templateID = "template_je714f6";
+
+    emailjs
+        .send(serviceID, templateID, params)
+        .then((res) => {
+                document.getElementById("name").value = "";
+                document.getElementById("email").value = "";
+                document.getElementById("message").value = "";
+                console.log(res);
+                alert("Your message was sent successfully!");
+            })
+            .catch((err) => console.log(err));
+}
